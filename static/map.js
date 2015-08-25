46ee8528aee6c767368b1e23fde5a983dab0d5bd
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-24 18:35:40 -0700

    When you check share location, browser retrieves location, disables Post button and shows a loading message until location is loaded

--- static/map.js
+++ static/map.js
@@ -9,38 +9,20 @@ function findMe() {
       console.log(lat);
       $("#hideLng").val(lng);
       console.log(lng)
+      $("#geoMessage").html("");
+      $('#submitButton').removeAttr('disabled');
     });
   } else {
     // Browser doesn't support Geolocation
-	$('#noLocation').html("Unable to retrieve your location");
+	$('#geoMessage').html("Unable to retrieve your location");
   }
 }
 
 $("#geoCheckbox").click(function () {
 	var thisCheck = $(this);
     if(thisCheck.is (':checked')) {
+    	$('#submitButton').attr('disabled', 'disabled');
+    	$("#geoMessage").html("...Waiting for location...");
     	findMe();
     }
 });
-
-function waitForLocationAndSubmit(evt) {
-	evt.preventDefault();
-	var checkbox = $("#geoCheckbox")
-	if(checkbox.is (':checked')) {
-		var lngInput = $("#hideLng");
-		console.log(lngInput)
-		var lngVal = lngInput.val();
-		var i = 0;
-		while (lngVal === "" && i<100000000){
-		// 	lngVal = lngInput.val();
-		// 	i++;
-			// dont submit
-		// }
-	}
-	// this.submit()
-}
-
-$("#postForm").on('submit', waitForLocationAndSubmit);
-
-// In findMe... before fxn callback, disable submit button, and display "waiting for location"
-// incallback enable submit
