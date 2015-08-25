adad19a2a2b7d64caf893953002299a8cf0b97c0
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-25 13:12:35 -0700

    modified my sample data so it has slightly different lats/longs

--- static/map.js
+++ static/map.js
@@ -1,10 +1,11 @@
 var map;
+var allInfos = [];
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.773561, lng: -122.444323},
     zoom: 13
   });
-
+  
   $.get('/listings.json', function(data) {
     var image = 'static/images/mlmlogo.jpg';
     for (var key in data) {
@@ -17,12 +18,13 @@ function initMap() {
               '<p>Date Posted: ' + location['date_posted'] + '</p>'+
               '</div>'
             );
-            console.log(contentString);
 
             // Create info window 
             var infoWindow = new google.maps.InfoWindow({
               content: contentString
             });
+            //Keep track of all the infoWindows.
+            allInfos.push(infoWindow)
 
             // Create marker per location object
             marker = new google.maps.Marker({
@@ -42,7 +44,14 @@ function initMap() {
 function bindinfoWindow(marker, map, infoWindow, html) {
   google.maps.event.addListener(marker, 'click', function() {
     // Set infoWindow content and open it when user clicks.
+    closeInfos();
     infoWindow.setContent(html);
     infoWindow.open(map, marker);
   });
+}
+
+function closeInfos() {
+        for (i = 0; i < allInfos.length; i++) {
+            allInfos[i].close();
+        }
 }
