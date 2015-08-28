017eebb8667842b5109fc350be8cf6f12d39b809
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-27 17:18:51 -0700

    working on jasmine

--- static/messages-spec.js
+++ static/messages-spec.js
@@ -1,30 +1,13 @@
 describe("delete message function", function () {
-	// beforeEach(function() {
- //      jasmine.Ajax.install();
- //    });
- //    afterEach(function() {
- //    	jasmine.Ajax.uninstall();
- //    });
 
-    it("should make an AJAX request to the correct URL", function() {
-    spyOn($, "post");
+    it("should call the function when the button is clicked", function() {
 
-    expect($.post).toHaveBeenCalled()
+    var fakeElement = $("<button class='deleteButton btn btn-xs' data-id='delete42'>Delete</button></");
+    spyOn(fakeElement, 'deletemessage');
+    expect(fakeElement.deleteMessage).toHaveBeenCalled()
+
 
     });
-    
-    // deleteMessageTest();
-//     expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("/delete_message");
-// 	});
-
-// function deleteMessageTest() {
-//     $.post("/delete_message", messageId, function (result) {
-// 		$("#messageSent").html('');
-// 		$("#messageSent").css("display", "block");
-// 		$("#messageSent").html('Your message has been deleted.');
-// 		$("#messageSent").delay(3000).fadeOut();
-// 		$("#message"+thisMessage).remove();
-// 		$("#messageCount").html(result['new_messages']);
-// 		});
 //     }//closes deleteMessageTest
 });//close describe
+
