5a9781def01dda4729fd3b270fed22d0be04db96
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-29 13:08:33 -0700

    seed.py pep8 ok

--- seed.py
+++ seed.py
@@ -1,34 +1,56 @@
-"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+# Utility file to seed ratings database from MovieLens data in seed_data
 
-from model import connect_to_db, User, Friendship, Food, Message, Allergen, Location
+from model import (connect_to_db, User,
+                   Friendship, Food, Message,
+                   Allergen, Location)
 from server import app
 from datetime import datetime
 
 
 def load_users():
-    """Load users from sampleusers into database."""
+    # Load users from sampleusers into database.
 
     user_file = open('sampledata/sampleusers.txt')
-    
+
     for line in user_file:
-        email, password, fname, lname, fbid= line.split("|")
+        email, password, fname, lname, fbid = line.split("|")
         fbid = int(fbid)
         User.add_user(email, fname, lname, password, fbid)
 
+
 def load_listings():
-    """Load listings from samplelistings into database"""
+    # Load listings from samplelistings into database
 
     listings_file = open('sampledata/samplelistings.txt')
 
     for line in listings_file:
-        title, texture, datemade, quantity, freshfrozen, description, allergen_list, user_id, phone_number, lat, lng = line.split("|")
+        (title,
+         texture,
+         datemade,
+         quantity,
+         freshfrozen,
+         description,
+         allergen_list,
+         user_id,
+         phone_number,
+         lat,
+         lng) = line.split("|")
         allergen = Allergen.add_allergen(allergen_list)
         location = Location.add_location(lat, lng)
-        Food.add_food(title, texture, datemade, quantity,
-                 freshfrozen, description, allergen.allergen_id, user_id, phone_number, location)
+        Food.add_food(title,
+                      texture,
+                      datemade,
+                      quantity,
+                      freshfrozen,
+                      description,
+                      allergen.allergen_id,
+                      user_id,
+                      phone_number,
+                      location)
+
 
 def load_messages():
-    """Load messages from samplemessages into database"""
+    # Load messages from samplemessages into database
 
     messages_file = open('sampledata/samplemessages.txt')
 
@@ -36,7 +58,7 @@ def load_messages():
         line = line.rstrip()
         sender_id, receiver_id, message_sent = line.split("|")
         Message.add_message(sender_id, receiver_id, message_sent)
-        
+
 if __name__ == "__main__":
     connect_to_db(app)
 
