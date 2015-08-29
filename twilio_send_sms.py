2364ec20cd0a94680ca8661c84a254774ee346f6
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-29 13:19:26 -0700

    twillio_send_sms.py pep8 ok

--- twilio_send_sms.py
+++ twilio_send_sms.py
@@ -1,18 +1,22 @@
 from twilio.rest import TwilioRestClient
 import os
 
-TWILIO_ACCOUNT_SID=os.environ['TWILIO_ACCOUNT_SID']
-TWILIO_AUTH_TOKEN=os.environ['TWILIO_AUTH_TOKEN']
-TWILIO_NUMBER=os.environ['TWILIO_NUMBER']
+TWILIO_ACCOUNT_SID = os.environ['TWILIO_ACCOUNT_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
 
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+
 def send_text(to_number, message):
-	to_number = to_number
-	body_message = message
+    # takes a telephone number and a message
+    # sends a message from twilio number
 
-	message = client.messages.create(from_=TWILIO_NUMBER, 
-									 to=to_number, 
-									 body=body_message)
+    to_number = to_number
+    body_message = message
 
-	print message.sid
+    message = client.messages.create(from_=TWILIO_NUMBER,
+                                     to=to_number,
+                                     body=body_message)
+
+    print message.sid
