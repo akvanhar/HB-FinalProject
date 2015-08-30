5a6664cade4a441397b031c166ad74ba435c8971
Alyson van Hardenberg <avanhardenberg@gmail.com>
2015-08-29 21:00:22 -0700

    server almost pep8 ok

--- server.py
+++ server.py
@@ -4,14 +4,17 @@ import json
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, render_template, redirect, request, flash, session, jsonify
+from flask import (Flask, render_template, redirect,
+                   request, flash, session, jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
 
 from datetime import datetime
 
 from sqlalchemy import desc
 
-from model import db, connect_to_db, User, Friendship, Food, Message, Allergen, Location
+from model import (db, connect_to_db, User,
+                   Friendship, Food, Message,
+                   Allergen, Location)
 
 from titlecase import titlecase
 
@@ -49,38 +52,51 @@ def home():
         user_friends = user.friendships
 
         if user_friends:
-            friend_ids = [friend.friend_id for friend in user_friends] #get this user's friend ids
-            friends_fb_ids = db.session.query(User.user_id, User.fb_id).filter(User.user_id.in_(friend_ids)).all()
+            # get this user's friend ids
+            friend_ids = [friend.friend_id for friend in user_friends]
+            friends_fb_ids = db.session.query(User.user_id,
+                                              User.fb_id).filter(
+                                              User.user_id.in_(friend_ids)
+                                              ).all()
 
-            #get all their friend's listings
+            # get all their friend's listings
             friends_listings = []
             for friend in friend_ids:
-                listing = Food.query.filter_by(active=1).filter(Food.user_id.in_(friend_ids)).order_by(desc('post_date')).first()
+                listing = Food.query.filter_by(active=1
+                                               ).filter(
+                                               Food.user_id.in_(friend_ids)
+                                               ).order_by(desc('post_date')
+                                                          ).first()
                 friends_listings.append(listing)
 
-            #get the food ids so they can be filtered out
+            # get the food ids so they can be filtered out
             friends_food_ids = [food.food_id for food in friends_listings]
 
-            #get all the other active listings
-            other_listings = Food.query.filter_by(active=1).filter((~Food.food_id.in_(friends_food_ids)), ~(Food.user_id==session['user_id'])).order_by(desc('post_date')).all()
+            # get all the other active listings
+            other_listings = Food.query.filter_by(active=1
+                                                  ).filter(
+                                                  (~Food.food_id.in_(friends_food_ids)),
+                                                  (~Food.user_id == session['user_id'])
+                                                  ).order_by(desc('post_date')).all()
 
-            #combine listings so that the friends listings come first
+            # combine listings so that the friends listings come first
             this_users_listings = friends_listings + other_listings
             short_list = this_users_listings[:5]
-        
         else:
-            short_list = Food.query.filter_by(active=1).order_by(desc('post_date')).limit(5).all()
+            short_list = Food.query.filter_by(active=1
+                                              ).order_by(desc('post_date')
+                                                         ).limit(5).all()
             friends_fb_ids = None
 
         current_date = datetime.now()
         current_date = current_date.strftime("%Y-%m-%d")
 
-        return render_template('index.html', 
-                               user_listings=short_list, 
-                               current_date = current_date, 
-                               user=user, 
+        return render_template('index.html',
+                               user_listings=short_list,
+                               current_date=current_date,
+                               user=user,
                                new_messages=new_messages,
-                               user_friends = user_friends,
+                               user_friends=user_friends,
                                friends_fb_ids=friends_fb_ids)
 
 
@@ -123,7 +139,6 @@ def facebook_login():
     fb_friends = json.loads(fb_friends)
     fb_user = User.query.filter_by(fb_id=fb_user_id).first()
 
-
     if fb_user:
         # User has previously logged into MLM
         user_id = fb_user.user_id
@@ -135,13 +150,18 @@ def facebook_login():
             friends_user_ids = []
             # turn the fb_ids into user_ids.
             for friend_fb_id in fb_friends:
-                friend_user_id = db.session.query(User.user_id).filter_by(fb_id=friend_fb_id).first()
+                friend_user_id = db.session.query(User.user_id
+                                                  ).filter_by(
+                                                  fb_id=friend_fb_id
+                                                  ).first()
                 friends_user_ids.append(friend_user_id)
             friends_user_ids = [x[0] for x in friends_user_ids]
 
             # now see if those friends are in the friendship table.
             for friend in friends_user_ids:
-                friend_exists = db.session.query(Friendship.friend_id).filter_by(friend_id=friend).first()
+                friend_exists = db.session.query(Friendship.friend_id
+                                                 ).filter_by(friend_id=friend
+                                                             ).first()
                 # if they're not, add them in! Yay friendship!
                 if friend_exists is None:
                     Friendship.add_friendship(user_id, friend)
@@ -152,37 +172,41 @@ def facebook_login():
     else:
         # First time for user logging into MLM
         # add the user to the database
-        User.add_user(email=fb_email, fname=fb_fname, lname=fb_lname, fb_id=fb_user_id)
+        User.add_user(email=fb_email, fname=fb_fname,
+                      lname=fb_lname, fb_id=fb_user_id)
         # access that user's information, add it to the session
         fb_user = User.query.filter_by(fb_id=fb_user_id).first()
         user_id = fb_user.user_id
-        
+
         session['user_id'] = user_id
         session['current_acces_token'] = current_acces_token
-        
+
         flash('Thanks for creating an account with Make Less Mush')
-        
+
         return redirect('/home')
+
 
 @app.route('/logbutton')
 def logbutton():
     # You get here if you click the login/logout button from any page
     # other than login/signup
-    
+
     if 'user_id' in session:
         del session['user_id']
         flash("Logout successful!")
     if 'current_acces_token' in session:
         del session['current_acces_token']
         flash("You have logged out of Facebook")
-     
+
     return redirect("/login")
+
 
 @app.route('/signup')
 def signup():
     # Allows a user to signup for Make Less Mush
 
     return render_template('signup.html')
+
 
 @app.route('/signup_portal', methods=['POST'])
 def signup_portal():
@@ -206,12 +230,14 @@ def signup_portal():
 
     return redirect('/home')
 
+
 @app.route('/fblogout_portal')
 def logout_portal():
     # Handles logout of MLM
     # FIXTHIS
 
     return redirect('/login')
+
 
 @app.route('/postlisting', methods=['POST'])
 def postlisting():
@@ -245,11 +271,14 @@ def postlisting():
     else:
         location_id = None
 
-    Food.add_food(title, texture, datemade, quantity, freshfrozen, description, allergen_id, user_id, location_id, phone_number)
+    Food.add_food(title, texture, datemade, quantity, freshfrozen,
+                  description, allergen_id, user_id, location_id,
+                  phone_number)
 
     flash('Your mush has been successfully posted!')
 
     return redirect('/home')
+
 
 @app.route('/listings.json')
 def display_listings():
@@ -268,7 +297,8 @@ def display_listings():
         for location in location_results:
             locations[location.location_id] = {
                 "title": (location.foods[0]).title,
-                "date_posted": (location.foods[0]).post_date.strftime("%Y-%m-%d"),
+                "date_posted": (location.foods[0]
+                                ).post_date.strftime("%Y-%m-%d"),
                 "posting_user": (location.foods[0]).user.fname,
                 "posting_user_id": (location.foods[0]).user_id,
                 "food_id": (location.foods[0]).food_id,
@@ -277,6 +307,7 @@ def display_listings():
             }
 
     return jsonify(locations)
+
 
 @app.route('/listings')
 def listings():
@@ -292,26 +323,42 @@ def listings():
         user_friends = user.friendships
 
         if user_friends:
-            friend_ids = [friend.friend_id for friend in user_friends] #get this user's friend ids
+            friend_ids = [friend.friend_id for friend in user_friends]
 
             # get all their friend's listings
-            friends_listings = Food.query.filter_by(active=1).filter(Food.user_id.in_(friend_ids)).order_by(desc('post_date')).all() 
+            friends_listings = Food.query.filter_by(active=1
+                                                    ).filter(
+                                                    Food.user_id.in_(friend_ids)
+                                                    ).order_by(
+                                                    desc('post_date')
+                                                    ).all()
 
             # get the food ids so they can be filtered out
             friends_food_ids = [food.food_id for food in friends_listings]
 
             # get all the other active listings
-            other_listings = Food.query.filter_by(active=1).filter(~Food.food_id.in_(friends_food_ids)).order_by(desc('post_date')).all()
+            other_listings = Food.query.filter_by(active=1
+                                                  ).filter(~Food.food_id.in_(
+                                                   friends_food_ids)
+                                                   ).order_by(desc('post_date')
+                                                              ).all()
 
             # combine listings so that the friends listings come first
             foods = friends_listings + other_listings
 
         else:
-            foods = Food.query.filter_by(active=1).order_by(desc('post_date')).all()
+            foods = Food.query.filter_by(active=1).order_by(desc
+                                                            ('post_date'
+                                                             )).all()
 
         API_KEY = google_api
 
-        return render_template('listings.html', foods=foods, user=user, new_messages=new_messages, API_KEY=API_KEY)
+        return render_template('listings.html',
+                               foods=foods,
+                               user=user,
+                               new_messages=new_messages,
+                               API_KEY=API_KEY)
+
 
 @app.route('/listings/<int:food_id>')
 def food_info(food_id):
@@ -320,12 +367,16 @@ def food_info(food_id):
     if check_login('Please login to view listing details.') == 'not_logged_in':
         return redirect('/login')
     else:
-        #get specific listing from db.
+        # get specific listing from db.
         food_listing = Food.query.get(food_id)
         user = get_user()
         new_messages = get_new_messages(session['user_id'])
 
-        return render_template('food_info.html', food_listing=food_listing, user=user, new_messages=new_messages)
+        return render_template('food_info.html',
+                               food_listing=food_listing,
+                               user=user,
+                               new_messages=new_messages)
+
 
 @app.route('/mylistings')
 def user_listings():
@@ -334,17 +385,24 @@ def user_listings():
     if check_login('Please login to view your listings.') == 'not_logged_in':
         return redirect('/login')
     else:
-        #show user's listings.
+        # show user's listings.
         user = get_user()
         new_messages = get_new_messages(session['user_id'])
         user_id = session['user_id']
-        active_user_listings = Food.query.filter(Food.user_id == user_id, Food.active == 1).order_by(desc('post_date')).all()
-        old_listings = Food.query.filter(Food.user_id == user_id, Food.active == 0).order_by(desc('post_date')).all()
+        question = Food.query
+        user_listings = question.filter(Food.user_id == user_id,
+                                        Food.active == 1
+                                        ).order_by(desc('post_date')).all()
+        old_listings = question.filter(Food.user_id == user_id,
+                                       Food.active == 0
+                                       ).order_by(desc('post_date')).all()
 
-        return render_template('mylistings.html', 
-                                user_listings=active_user_listings, 
-                                old_listings=old_listings, user=user, 
-                                new_messages=new_messages)
+        return render_template('mylistings.html',
+                               user_listings=user_listings,
+                               old_listings=old_listings,
+                               user=user,
+                               new_messages=new_messages)
+
 
 @app.route('/listings/edit/<int:food_id>')
 def edit_food(food_id):
@@ -359,7 +417,11 @@ def edit_food(food_id):
         new_messages = get_new_messages(session['user_id'])
         food_listing = Food.query.get(food_id)
 
-        return render_template('editfood.html', food_listing=food_listing, user=user, new_messages=new_messages)
+        return render_template('editfood.html',
+                               food_listing=food_listing,
+                               user=user,
+                               new_messages=new_messages)
+
 
 @app.route('/updatelisting', methods=['POST'])
 def update_listing():
@@ -388,30 +450,37 @@ def update_listing():
     this_allergen = Allergen.query.get(allergen_id)
     this_allergen.update_allergen(allergen_id, allergen_list)
 
-    shared_with = shared_with_fname +" "+ shared_with_lname
+    shared_with = shared_with_fname + " " + shared_with_lname
 
     this_food = Food.query.get(food_id)
     this_food.update_food(food_id, title, texture, datemade, quantity,
-             freshfrozen, description, active, shared_with)
+                          freshfrozen, description, active, shared_with)
 
     flash('Your listing has been successfully updated!')
 
     return redirect('/mylistings')
 
+
 @app.route('/user/<int:food_user_id>')
 def user_info(food_user_id):
     # Displays a specific user's active listings.
 
-    if check_login("Please login to view this user's listings.") == 'not_logged_in':
+    if check_login("Please login to view\
+                   this user's listings.") == 'not_logged_in':
         return redirect('/login')
     else:
-        #get specific listing from db.
+        # get specific listing from db.
         user = get_user()
         new_messages = get_new_messages(session['user_id'])
         this_user = User.query.get(food_user_id)
-        food_listings = Food.query.filter_by(user_id = food_user_id)
+        food_listings = Food.query.filter_by(user_id=food_user_id)
 
-        return render_template('user_info.html', this_user=this_user, food_listings=food_listings, user=user, new_messages=new_messages)
+        return render_template('user_info.html',
+                               this_user=this_user,
+                               food_listings=food_listings,
+                               user=user,
+                               new_messages=new_messages)
+
 
 @app.route('/messages')
 def messages():
@@ -430,12 +499,13 @@ def messages():
 
         all_messages = unread_messages + read_messages
 
-        return render_template('messages.html', 
-            all_messages=all_messages, 
-            unread_messages=unread_messages, 
-            read_messages=read_messages, 
-            user=user,
-            new_messages=new_messages)
+        return render_template('messages.html',
+                               all_messages=all_messages,
+                               unread_messages=unread_messages,
+                               read_messages=read_messages,
+                               user=user,
+                               new_messages=new_messages)
+
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -448,19 +518,24 @@ def send_message():
 
     Message.add_message(current_user_id, posting_user, message)
 
-    # Send an alert via sms to the posting user that someone is interested in their food.
+    # Send an alert via sms to the posting
+    # user that someone is interested in their food.
     food_listing_id = request.form.get('food_listing')
     food_listing = Food.query.get(food_listing_id)
     poster_name = food_listing.user.fname
-    
+
     phone_number = food_listing.phone_number
     if phone_number:
         phone_number = "+1"+phone_number
-        send_text(phone_number, "Hi, %s! Someone's interested in your post on Make Less Mush! Sign in and check your messages!" % (poster_name))
+        send_text(phone_number,
+                  "Hi, %s! Someone's interested in your post\
+                  on Make Less Mush! Sign in and check your\
+                  messages!" % (poster_name))
 
     flash('Your message has been sent.')
 
     return redirect('/home')
+
 
 @app.route('/delete_message', methods=['POST'])
 def change_read_status():
@@ -475,6 +550,7 @@ def change_read_status():
 
     return jsonify(new_messages=new_messages)
 
+
 @app.route('/reply_to_message', methods=['POST'])
 def reply_to():
     # Reply to a message.
@@ -487,6 +563,7 @@ def reply_to():
 
     return "Your message has been sent."
 
+
 @app.route('/send_text', methods=['POST'])
 def send_sms_message():
     # Send an sms to a user about their listing.
@@ -496,6 +573,7 @@ def send_sms_message():
     send_text(number, message)
 
     return "Message sent"
+
 
 @app.route('/toggle_read', methods=['POST'])
 def toggle_read():
@@ -508,7 +586,7 @@ def toggle_read():
 
     return jsonify(read_status=message.read_status, new_messages=new_messages)
 
-################################################################################
+###############################################################################
 
 if __name__ == "__main__":
     # Set debug to true to have the toolbar extension run.
